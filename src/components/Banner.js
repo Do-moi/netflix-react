7acb8c9f305d232db442a9fa8c2200127e264323
Marc Do <domarc251@gmail.com>
2023-07-11 14:57:56 +0200

    test public

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,7 +14,7 @@ function Banner() {
       const request = await fetch(
         // "https://backendnetflixclone.herokuapp.com/load-movies-banner"
         // "http://localhost:3000/load-movies-banner",
-        "http://172.20.3.190:3000/load-movies-banner",
+        "http://54.193.21.248:3000/load-movies-banner",
         {
           method: "GET",
 
